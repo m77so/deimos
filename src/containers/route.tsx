28e8f8f71333d900394475ff5226125924b62c1f
m77so <m77code@gmail.com>
2017-11-02 18:58:55 +0900

    勉強中２

---
+++ src/containers/route.tsx
@@ -0,0 +1,7 @@
+import {Route} from '../modules/route'
+import {connect} from 'react-redux'
+import {Dispatch} from 'redux'
+
+export class ActionDispatcher {
+  
+}
--- src/modules/route.tsx
+++ src/modules/route.tsx
@@ -1,6 +1,28 @@
-export const SET_SRC_STATION = 'SET_SRC_STATION'
-export const SET_DEST_STATION = 'SET_DEST_STATION'
+import { Action } from 'redux'
 
+enum ActionNames {
+  SET_DEST = 'SET_DEST_STATION',
+  SET_SRC = 'SET_SRC_STATION',
+  CHANGE_LINE = 'CHANGE_LINE'
+}
+
+interface SetDestAction extends Action {
+  type: ActionNames.SET_DEST
+  station: string
+}
+export const setDestStation = (station: string): SetDestAction => ({
+  type: ActionNames.SET_DEST,
+  station: station
+})
+
+interface SetSrcAction extends Action {
+  type: ActionNames.SET_SRC
+  station: string
+}
+export const setSrcStation = (station: string): SetSrcAction => ({
+  type: ActionNames.SET_SRC,
+  station: station
+})
 export interface RouteState {
   source: string,
   destination: string,
@@ -12,6 +34,18 @@ const initialState: RouteState = {
   via: []
 }
 
-export default function reducer(state: RouteState = initialState, action: string){
-
+export type RouteActions = SetDestAction | SetSrcAction
+export default function reducer(state: RouteState = initialState, action: RouteActions ) {
+  switch (action.type) {
+    case ActionNames.SET_SRC:
+      return Object.assign({}, state, {
+        source: action.station
+      })
+    case ActionNames.SET_DEST:
+      return Object.assign({}, state, {
+        destination: action.station
+      })
+    default:
+      return state
+  }
 }
