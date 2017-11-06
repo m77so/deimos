26e23ad9e8a5a039f2bb28bf7dad2bcc4cc5119a
m77so <m77code@gmail.com>
2017-11-06 22:09:40 +0900

    テキストのみ対応とREADME

--- src/counter/module.ts
+++ src/counter/module.ts
@@ -1,29 +1,8 @@
 import { Action } from 'redux'
 
 enum ActionNames {
-  SOURCE = 'route/source',
-  DESTINATION = 'route/decrement',
   TEXT = 'route/text'
 }
-
-interface SourceAction extends Action {
-  type: ActionNames.SOURCE
-  station: string
-}
-export const setSourceStation = (station: string): SourceAction => ({
-  type: ActionNames.SOURCE,
-  station: station
-})
-
-interface DestinationAction extends Action {
-  type: ActionNames.DESTINATION
-  station: string
-}
-
-export const setDestinationStation = (station: string): DestinationAction => ({
-  type: ActionNames.DESTINATION,
-  station: station
-})
 
 interface TextAction extends Action {
   type: ActionNames.TEXT
@@ -42,7 +21,7 @@ export interface RouteState {
   text: string
 }
 
-export type RouteActions = SourceAction | DestinationAction | TextAction
+export type RouteActions = TextAction
 
 const initialState: RouteState = { 
   source: '金沢', destination: '福井', sourceValid: false, destinationValid: false, text: '金沢 福井' 
@@ -54,12 +33,6 @@ export default function reducer(
 ): RouteState {
   const copyState = Object.assign({}, state)
   switch (action.type) {
-    case ActionNames.SOURCE:
-      copyState.source = action.station
-      break
-    case ActionNames.DESTINATION:
-      copyState.destination = action.station
-      break
     case ActionNames.TEXT:
       copyState.text = action.text
       const textArray = action.text.split(' ')
