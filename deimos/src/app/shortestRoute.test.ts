785119012ed00b3866bd56a0c0e989cd03e7dca9
m77so <m77code@gmail.com>
2017-11-26 20:57:16 +0900

    最短路　テストの充実

--- deimos/src/app/shortestRoute.test.ts
+++ deimos/src/app/shortestRoute.test.ts
@@ -1,8 +1,30 @@
 import * as shortestRoute from './shortestRoute'
 import { data } from './data'
-it('same line', () => {
-  const startStation = data.stations[data.stationNames.indexOf('金沢')]
-  const endStation = data.stations[data.stationNames.indexOf('芦原温泉')]
-  const s = shortestRoute.default(startStation, endStation)
-  expect(s.stations[0].name).toBe('金沢')
+import { Route } from './route'
+const testFunction = (start: string, end: string): Route => {
+  const startStation = data.stations[data.stationNames.indexOf(start)]
+  const endStation = data.stations[data.stationNames.indexOf(end)]
+  return shortestRoute.default(startStation, endStation)
+}
+it('隣駅の最短検索', () => {
+  const s = testFunction('西金沢', '野々市')
+  expect(s.edges.filter(edge => edge.line.name !== '北陸').length).toBe(0)
+  expect(s.edges.length).toBe(1)
+})
+it('同一路線の最短検索', () => {
+  const s = testFunction('金沢', '芦原温泉')
+  expect(s.edges.filter(edge => edge.line.name === '北陸').length).toBeGreaterThan(0)
+  expect(s.edges.filter(edge => edge.line.name !== '北陸').length).toBe(0)
+})
+it('開始駅、終点駅が一致するか', () => {
+  const stations = ['西金沢', '野々市', '法隆寺', '大聖寺', '南小倉']
+  for (const stationA of stations) {
+    for (const stationB of stations) {
+      if (stationA !== stationB) {
+        const s = testFunction(stationA, stationB)
+        expect(s.stations[0].name).toBe(stationA)
+        expect(s.stations[s.stations.length - 1].name).toBe(stationB)
+      }
+    }
+  }
 })
